d49af565ea6da3d4136f5d51697f60422a51d038
Caleb Williams <geek_williams@hotmail.com>
2022-03-07 21:00:23 -0700

    Unmount is not working

--- services/automount.js
+++ services/automount.js
@@ -344,13 +344,15 @@ function automountService(blacklist, mounted){
                         else { 
                             logWrite("Successfully unmounted device at " + d.mountPoint, 'info', log).catch(err => { console.error("Unable to write to log.  Does it exist?"); console.error(err)});
                             // return list of mounts to main class when finished
-                            resolve(mList.splice(i, 1))
+                            mList.splice(i, 1)
                         }
                     }
                 }
                 else { 
+                    resolve(mList);
                     // found uuid, no need to umount 
                 }
+                resolve(mList);
             }
         }   
         catch (err){ 
